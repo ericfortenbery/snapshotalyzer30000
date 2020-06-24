b0478a147ccaf9885e393006483e8c7556dfb1b1
unknown <ericfortenbery@gmail.com>
2020-06-23 20:56:50 -0600

    List volumes and snapshot

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -19,6 +19,32 @@ def filter_instances(project):
 def cli():
     """Shotty manages snapshots"""
 
+@cli.group('snapshots')
+def snapshots():
+    """Commnad for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only snapshots for project (tag Project:<name>)")
+def list_snapshots(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+
+    return
+
 @cli.group('volumes')
 def volumes():
     """Commands for volumes"""
@@ -46,6 +72,22 @@ def list_volumes(project):
 @cli.group('instances')
 def instances():
     """Commands for instances"""
+
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    "Create snapshots for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by Snapshotalyzer 30000")
+
+    return
 
 @instances.command('list')
 @click.option('--project', default=None,
